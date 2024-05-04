18b988a443dd2427674b70c29e567905186c7839
Nilanchal <nilanchalpanda2003@gmail.com>
2024-05-04 17:31:11 +0530

    Features part added

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,18 +1,17 @@
 function Hero() {
   return (
-    <div className='bg-gradient-to-t mt-20  h-[100vh]'>
-      <div className='flex flex-col md:gap-4 text-center font-bold text-6xl '>
-      <h1 className="text-slate-200">
-        Gain your market adavantage with
-      </h1>
-      <h1 className='bg-gradient-to-r from-primary to-secondary inline-block text-transparent bg-clip-text'>Customized digital solutions</h1>
-      <div className=' bg-gradient-to-b from-black via-secondary'>
-        <img src='../images/image2.png' className="w-100 mx-auto "
-/>
-      </div>
+    <div className="bg-gradient-to-t mt-20  h-[100vh]">
+      <div className="flex flex-col md:gap-4 text-center font-bold text-4xl md:text-6xl ">
+        <h1 className="text-slate-200">Gain your market adavantage with</h1>
+        <h1 className="bg-gradient-to-r from-primary to-secondary inline-block text-transparent bg-clip-text">
+          Customized digital solutions
+        </h1>
+        <div className=" bg-gradient-to-b from-black via-secondary">
+          <img src="../images/image2.png" className="w-100 mx-auto " />
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
