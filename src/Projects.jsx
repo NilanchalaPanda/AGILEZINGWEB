66d3526b49a2adcdce152c00ce8f398e444f98c6
usmaan <2021.mohdusmaan.mogal@ves.ac.in>
2024-05-04 15:16:10 +0530

    Hero, cta, projects initialized

---
+++ src/Projects.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <div className="bg-black" >
+      <h2 className="text-center text-slate-200 text-4xl font-bold">Our work</h2>
+      <div className="grid grid-cols-2 mx-72 my-10 gap-8">
+        <div >
+          <img
+            alt=""
+            src="../images/image1.jpg"
+            className="h-20 w-full object-cover sm:h-80 lg:h-96"
+          />
+
+          <h3 className="mt-4 text-lg font-bold text-slate-200 sm:text-xl">
+            Project 1
+          </h3>
+
+          <p className="mt-2 max-w-sm text-slate-200">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni
+            reiciendis sequi ipsam incidunt.
+          </p>
+        </div>
+        <div className="block">
+          <img
+            alt=""
+            src="../images/image1.jpg"
+            className="h-20 w-full object-cover sm:h-80 lg:h-96"
+          />
+
+          <h3 className="mt-4 text-lg font-bold text-slate-200 sm:text-xl">
+            Project 1
+          </h3>
+
+          <p className="mt-2 max-w-sm text-slate-200">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni
+            reiciendis sequi ipsam incidunt.
+          </p>
+        </div>
+        <div className="block">
+          <img
+            alt=""
+            src="../images/image1.jpg"
+            className="h-20 w-full object-cover sm:h-80 lg:h-96"
+          />
+
+          <h3 className="mt-4 text-lg font-bold text-slate-200 sm:text-xl">
+            Project 1
+          </h3>
+
+          <p className="mt-2 max-w-sm text-slate-200">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni
+            reiciendis sequi ipsam incidunt.
+          </p>
+        </div>
+        <div className="block">
+          <img
+            alt=""
+            src="../images/image1.jpg"
+            className="h-20 w-full object-cover sm:h-80 lg:h-96"
+          />
+
+          <h3 className="mt-4 text-lg font-bold text-slate-200 sm:text-xl">
+            Project 1
+          </h3>
+
+          <p className="mt-2 max-w-sm text-slate-200">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni
+            reiciendis sequi ipsam incidunt.
+          </p>
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: "#561DE4",
+        secondary: "#A961FD",
+        tertiary: "#5E2F71"
+      }
+    },
   },
   plugins: [],
 };
