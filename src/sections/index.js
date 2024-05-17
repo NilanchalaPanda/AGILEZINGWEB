3d1c1f665a2e4c4a122f74d88ff9a2bb9cb1a9ba
Nilanchal <nilanchalpanda2003@gmail.com>
2024-05-17 16:34:16 +0530

    Deleted react-app

--- src/sections/index.js
+++
@@ -1,6 +0,0 @@
-// import CTAText from "./CTAText";
-// import Hero from "./Hero";
-// import Projects from "./Projects";
-// import Footer from "./Footer";
-
-// export default { CTAText, Hero, Projects, Footer }
